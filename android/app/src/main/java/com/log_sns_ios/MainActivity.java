26d8a2d60f68cd1a391350d20074174f653c1260
suchenglong <chenglong.su@myxxjs.com>
2019-11-25 12:55:18 +0800

    创建项目

---
+++ android/app/src/main/java/com/log_sns_ios/MainActivity.java
@@ -0,0 +1,15 @@
+package com.log_sns_ios;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "log_sns_ios";
+  }
+}
